f99b44024b5854ed61a8c1a34d9e6942388730ca
Richard Marsot <richard.marsot@pega.com>
2024-01-02 15:41:47 -0500

    remove unneeded files

--- lib/components/Pega_Extensions_FieldGroupAsRow/styles.d.ts
+++
@@ -1,3 +0,0 @@
-declare const _default: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
-export default _default;
-//# sourceMappingURL=styles.d.ts.map
