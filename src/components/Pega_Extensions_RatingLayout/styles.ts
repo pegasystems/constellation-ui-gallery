ef05580c41f39697f4db66393a4e8d497c1d0d5f
Richard Marsot <richard.marsot@pega.com>
2024-03-22 19:32:43 -0400

    fix Rating component to use auto-fill to avoid stretching 100% widht

--- src/components/Pega_Extensions_RatingLayout/styles.ts
+++ src/components/Pega_Extensions_RatingLayout/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 export default styled.div(({ minWidth }: { minWidth: string }) => {
   return css`
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(${minWidth}, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(${minWidth}, 1fr));
     grid-gap: 1rem;
     padding-top: 1rem;
   `;
