09162f7dd11f52fe6e565bfc4c293660941adcb7
Richard Marsot <richard.marsot@pega.com>
2024-09-12 18:54:20 -0400

    fix for compare table radio-card to limit width of each card

--- src/components/Pega_Extensions_CompareTableLayout/styles.ts
+++ src/components/Pega_Extensions_CompareTableLayout/styles.ts
@@ -4,7 +4,6 @@ export default styled.div(({ displayFormat }: { displayFormat: string }) => {
   switch (displayFormat) {
     case 'radio-button-card':
       return css`
-        color: red !important;
         & > fieldset > div {
           display: grid;
           grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
