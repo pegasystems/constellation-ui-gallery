dab750076749e5cad96f6bce5bf8e2369b69a4af
Richard Marsot <richard.marsot@pega.com>
2024-12-01 12:12:48 -0500

    add new Status badge component

---
+++ src/components/Pega_Extensions_StatusBadge/demo.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { composeStories } from '@storybook/react';
+import * as DemoStories from './demo.stories';
+
+const { Default } = composeStories(DemoStories);
+
+test('renders a Status badge component with default args', async () => {
+  render(<Default />);
+  expect(await screen.findAllByText('Open')).toHaveLength(2);
+});
