6c2b8193c780860fc903db78c42709a672c0d698
Richard Marsot <richard.marsot@pega.com>
2025-09-25 14:21:42 -0400

    fix unit test linting

--- src/components/Pega_Lib_AutoSave/demo.test.tsx
+++ src/components/Pega_Lib_AutoSave/demo.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { composeStories } from '@storybook/react';
 import * as DemoStories from './demo.stories';
--- src/components/Pega_Lib_PasswordInput/demo.test.tsx
+++ src/components/Pega_Lib_PasswordInput/demo.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/react';
 import * as DemoStories from './demo.stories';
