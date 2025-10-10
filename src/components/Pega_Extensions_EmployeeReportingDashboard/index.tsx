4c2aa23fc323aedeb175ca6b2fbe17f9307e0f47
Gurbinder <gurbinder.singh@bitsinglass.com>
2025-10-10 08:38:25 +0530

    index modified

--- src/components/Pega_Extensions_EmployeeReportingDashboard/index.tsx
+++ src/components/Pega_Extensions_EmployeeReportingDashboard/index.tsx
@@ -52,12 +52,10 @@ function EmployeeMonitoringDashboard(props: DashboardProps) {
   }, [context]);
 
   if (loading) return (
-    <>
     <div className="loader-container">
-      <div className="loader"></div>
+      <div className="loader" />
       <div className="loader-text">Loading...</div>
     </div>
-    </>
   )
 
   return (
